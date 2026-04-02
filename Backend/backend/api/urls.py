2387130a4f336134253f5c604b39203482d3647f
Biniyam Girma <software.bini30@gmail.com>
2026-04-02 12:38:15 +0300

    Implement API for enrollment

--- Backend/backend/api/urls.py
+++ Backend/backend/api/urls.py
@@ -7,5 +7,6 @@ router.register(r"departments", views.DepartmentViewSet, basename="department")
 router.register(r"courses", views.CourseViewSet, basename="course")
 router.register(r"semesters", views.SemesterViewSet, basename="semester")
 router.register(r"course-assignment", views.CourseAssignmentViewSet, basename="course-assignment")
+router.register(r"enrollment", views.EnrollmentViewSet, basename="enrollment")
 
 urlpatterns = router.urls
